8be9ab9239a9122d7c727f28a4ffed48a1af51a5
Luis mayta <luis.mayta@gmail.com>
2020-02-20 17:01:07 -0500

    url xml

--- app/Repositories/DSpace.php
+++ app/Repositories/DSpace.php
@@ -29,8 +29,8 @@ trait DSpace
         $urls = $this->UrlDSpaceOAI($cantidad,$tipo);
         foreach ($urls['maestro'] as $key => $url) {
             if ($key==0)DB::table('recursos')->truncate();
-            // $url = $this->curl_get_contents($url);
-            $xmlObj = simplexml_load_file($url);
+            $url = $this->curl_get_contents($url);
+            $xmlObj = simplexml_load_string($url);
             if ((string)$xmlObj->error=='No matches for the query') {
                 echo 'No matches for the query :'.$url."\n";
             } else {
@@ -171,14 +171,7 @@ trait DSpace
     }
     function curl_get_contents($url)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-        $html = curl_exec($ch);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
+        return file_get_contents($url);
     }
 
 }
