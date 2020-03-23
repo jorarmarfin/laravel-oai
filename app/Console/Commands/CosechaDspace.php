a6179bb682f4cbfc676d1687a2b5ca37de0921a9
Luis Mayta <luis.mayta@drinux.com>
2020-03-23 10:58:36 -0500

    parametro de consecha

--- app/Console/Commands/CosechaDspace.php
+++ app/Console/Commands/CosechaDspace.php
@@ -41,6 +41,6 @@ class CosechaDspace extends Command
      */
     public function handle()
     {
-        if ($this->option('p')) $this->homecontroller->cosecha($this->option('p'));
+        if ($this->option('p')) $this->homecontroller->cosecha($this->option('p'),1);
     }
 }
